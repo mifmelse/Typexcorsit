177accacf90d8308b2e282425f4c2c196a387950
mifmelse <miftahulcz3@gmail.com>
2022-05-22 21:29:50 +0700

    Import character dan terrain
    Menambahkan mekanisme player berjalan menggunakan klik mouse serta animasinya

---
+++ Type or Be Hunted/Assets/Script/cameraCintrillir.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraCintrillir : MonoBehaviour
+{
+    public Transform playerTransform;
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    public bool LookAtPlayer = false;
+
+    void Start()
+    {
+        _cameraOffset = transform.position - playerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPos = playerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        if (LookAtPlayer)
+        {
+            transform.LookAt(playerTransform);
+        }
+    }
+}
---
+++ Type or Be Hunted/Assets/Script/playerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playerController : MonoBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    private bool running = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 500))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+
+        if(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            running = false;
+        }
+        else
+        {
+            running = true;
+        }
+
+        animator.SetBool("run", running);
+    }
+}
