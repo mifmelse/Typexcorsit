3a718bfc1ee0ea20fccee2c42c3a48c83ad565df
hubbystrife <hubbystrife@gmail.com>
2022-06-02 12:13:09 +0700

    Tuyul  seribu bayangan

---
+++ Type or Be Hunted/Assets/Script/spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject[] Enemy;
+    private int zpos;
+    private int xpos;
+    private int EnemyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("SpawnEnemy");
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (EnemyCount < 10)
+        {
+            xpos = Random.Range(-493, -227);
+            zpos = Random.Range(-93, 17);
+            int rand = Random.Range(0,Enemy.Length);
+            Instantiate(Enemy[rand], new Vector3(xpos, 16, zpos), Quaternion.identity);
+            yield return new WaitForSeconds(2);
+            EnemyCount += 1;
+        }
+    }
+}
