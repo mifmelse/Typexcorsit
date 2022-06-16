b4dd6d2216b5aaf4f7cbf6adec0b4799d400ada1
hubbystrife <hubbystrife@gmail.com>
2022-06-16 16:32:56 +0700

    script ketempelan tuyul jadi lebih op

--- Type or Be Hunted/Assets/Script/enemymovement.cs
+++ Type or Be Hunted/Assets/Script/enemymovement.cs
@@ -9,24 +9,35 @@ public class enemymovement : MonoBehaviour
     public Animator anim;
     public float lookRadius = 50f;
     public UnityEngine.AI.NavMeshAgent enemy;
-    public Transform Player;
+    private GameObject[] gameObjects;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+        gameObjects = GameObject.FindGameObjectsWithTag("Player");
+        if (gameObjects.Length == 0)
+        {
+            Debug.Log("No game objects are tagged with 'Player'");
+        }
+        else{
+            Debug.Log("Game Object Name =" + gameObjects[0]);
+            player = gameObjects[0];
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        float distance = Vector3.Distance(Player.position, transform.position);
-        Vector3 temp = Player.transform.position;
+
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        Vector3 temp = player.transform.position;
         if (distance <= lookRadius)
         {
             temp.y =  transform.position.y;
             transform.LookAt(temp);
-            enemy.SetDestination(Player.position);
+            enemy.SetDestination(player.transform.position);
             if (temp != Vector3.zero  )
             {
                 anim.SetBool("Seek",true);
@@ -42,7 +53,5 @@ public class enemymovement : MonoBehaviour
         else {
             anim.SetBool("Attack",false);
         }
-        
-        
     }
 }
